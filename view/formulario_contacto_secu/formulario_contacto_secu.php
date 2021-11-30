b1d4085703a28f5d52f745042e8f42abdb061fba
super-jordan <j0rd4nshippuden@gmail.com>
2021-11-30 11:46:27 -0600

    parte de jordan terminada

---
+++ view/formulario_contacto_secu/formulario_contacto_secu.php
@@ -0,0 +1,52 @@
+                                                  <div class="form">
+
+                    <h1 style="text-shadow: 3px 3px 3px #000; text-align: center; margin-top: 10%;">¿¡Quieres saber mas!?</h1>
+					
+                    <p style=" text-align: center; font-size: 20px;">Ingresa tus datos para poder comentar</p>
+                                                        
+
+                    <div class=" formulario2">
+                    <label for="Nombre" class="form-label">Nombre Completo</label>
+                    <input type="text" class="form-control" id="Nombre" placeholder="Nombre y Apellido">
+                    </div>
+
+                    <div class=" formulario2">
+                    <label for="E-mail" class="form-label">E-mail</label>
+                    <input type="e-mail" class="form-control" id="E-mail" placeholder="micorreo@ejemplo.com">
+                    </div>
+
+                    <div class=" formulario2">
+                    <label for="Telèfono" class="form-label">Telèfono</label>
+                    <input type="text" class="form-control" id="Telèfono" placeholder="Telèfono 10 digitos">
+                    </div>
+  
+                    <div class=" formulario2">
+                    <label for="Plantel" class="form-label">Plantel</label>
+                    <input type="text" class="form-control" id="Plantel" placeholder="--Elige un Plantel--">
+                    </div>
+
+				<div class=" formulario2">
+					<h5>Elige tu genero:</h5>
+				 <div style="background: #fff;" >
+					<div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="Masculino">
+                    <label class="form-check-label" for="Masculino">Masculino</label>
+                    </div>
+
+                    <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="Femenino" checked>
+                    <label class="form-check-label" for="Femenino">Femenino</label>
+                    </div>
+				 </div>
+			    </div>
+ 
+
+                    <div class="formulario2">
+                    <label for="Mensaje" class="form-label">Comentar</label>
+                    <textarea class="form-control" placeholder="Ingresa tu comentario" id="Mensaje" rows="3"></textarea>
+                    </div>
+
+                    <div>
+					<a href="#" class="btn btn-primary btn-block">Enviar informacion</a>	  
+                    </div>
+                </div>
